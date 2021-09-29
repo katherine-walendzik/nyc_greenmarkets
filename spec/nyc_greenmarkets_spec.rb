183004111f9c3ad8306ac59b3186a536524133c4
Katherine Walendzik <katanwal@icloud.com>
2021-09-29 08:41:43 -0400

    initialize repo

---
+++ spec/nyc_greenmarkets_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe NycGreenmarkets do
+  it "has a version number" do
+    expect(NycGreenmarkets::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
