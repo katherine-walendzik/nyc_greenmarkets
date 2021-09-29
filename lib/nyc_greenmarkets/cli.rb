b8472fad84ce4a88d5a85e4a481ccae25d15c5e9
Katherine Walendzik <katanwal@icloud.com>
2021-09-29 13:51:23 -0400

    created classes

--- lib/nyc_greenmarkets/cli.rb
+++ lib/nyc_greenmarkets/cli.rb
@@ -2,6 +2,14 @@ class NycGreenmarkets::CLI
     
     def start
       puts "Welcome to NYC Greenmarkets!"  
+      puts "Please select the number corresponding to the borough of your choice:"
+      #list boroughs as numbered list
+    end
+
+    def list_greenmarkets
+        NycGreenmarkest::Greenmarkets.all.each.with_index(1) do |market, index|
+            puts "#{index}. #{market}"
+        end
     end
 
 end
