06408750b09c6ed75a27017a998190b88d4bb121
Katherine Walendzik <katanwal@icloud.com>
2021-09-29 13:37:49 -0400

    set up bin

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,8 +1,2 @@
-# frozen_string_literal: true
-
 require_relative "nyc_greenmarkets/version"
-
-module NycGreenmarkets
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require_relative "nyc_greenmarkets/cli"
---
+++ lib/nyc_greenmarkets/cli.rb
@@ -0,0 +1,7 @@
+class NycGreenmarkets::CLI
+    
+    def start
+      puts "Welcome to NYC Greenmarkets!"  
+    end
+
+end
