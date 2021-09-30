adb0fdf87fb51edfdc3a037a5c792e211f320e35
Katherine Walendzik <katanwal@icloud.com>
2021-09-30 12:03:49 -0400

    create list_borough method with test data

--- lib/nyc_greenmarkets/cli.rb
+++ lib/nyc_greenmarkets/cli.rb
@@ -1,12 +1,24 @@
 class NycGreenmarkets::CLI
     
     def call
-      puts "Welcome to NYC Greenmarkets!"  
-      puts "Please select the number corresponding to the borough of your choice:"
-      #list boroughs as numbered list
+        puts "Welcome to NYC Greenmarkets!" 
+        list_borough 
     end
 
-    def list_greenmarkets
+    def list_borough
+        puts "Please select the number corresponding to the borough of your choice:"
+        #scrape data
+        @boroughs = ["Manhattan", "Brooklyn", "Queens", "Bronx", "Staten Island"]
+        @boroughs.each.with_index(1) do |month, index|
+            puts "#{index}. #{month}"
+        end
+    end
+
+    def get_borough
+    
+    end
+
+    def list_greenmarkets(borough)
         NycGreenmarkest::Greenmarkets.all.each.with_index(1) do |market, index|
             puts "#{index}. #{market}"
         end
