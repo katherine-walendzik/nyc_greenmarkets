988dba43f706d088b0e657d63dae0576074f01d8
Katherine Walendzik <katanwal@icloud.com>
2021-10-05 15:05:38 -0400

    updated scraper calls

--- lib/top_wines/cli.rb
+++ lib/top_wines/cli.rb
@@ -3,21 +3,35 @@ class TopWines::CLI
     def call
         puts "\nWelcome to Wine Spectator's Top 100 Wines of 2020!" 
         start
+        get_user_input
+        list_wines
+        TopWines::Scraper.scrape_wines
     end
 
     def start
-        puts "\nPlease select the group of wines you would like to see: \n1-20\n21-40\n41-60\n61-80\n81-100"
+        puts "\nPlease select the group of wines you would like to see:" 
+
+        @groups = ["1-20", "21-40", "41-60", "61-80", "81-100"]
+
+        @groups.each.with_index(1) do |group, index| 
+            puts "#{index}. #{group}"
+        end
+    
         #get user input
     end
 
-    #def get_user_input
-    #    chosen_group = gets.strip.to_i
-    #    list_wines(chosen_group) if valid_input(chosen_group.to_i, @group)
-    #end
+    def get_user_input
+        chosen_group = gets.strip.to_i
+        list_wines(chosen_group) if valid_input(chosen_group, @groups)
+    end
+
+    def valid_input(input, data)
+        input.to_i <= data.length && input.to_i > 0
+    end 
 
     #def list_wines(chosen_group)
+    #    group = @groups[chosen_group - 1]
     #    puts "\nWines ranked #{group}:"
-    #    end
     #end
 
 end
--- lib/top_wines/scraper.rb
+++ lib/top_wines/scraper.rb
@@ -1,13 +1,27 @@
 class TopWines::Scraper
-
+# TopWines::Scraper.scrape_wines
     def self.scrape_wines
         doc = Nokogiri::HTML(open("https://top100.winespectator.com/lists/"))
+        
+        wine_block = doc.css("tr td")
         binding.pry
+        index = 0
+        wine_block.each do |block, index|
+
+        {
+            rank: wine_block.css(".rank").text,
+            winery_name: wine_block.css("span.sort-text").text,
+            wine_name: wine_block.css("div.table-name span.wineName").children[1].text,
+            vintage: wine_block.css("td.vintage").text,
+            score: wine_block.css("td.score").text,
+            price: wine_block.css("td.price").text,
+            full_description: wine_block.css("div.tabel-note").children[0].text.strip.chomp(' —')
+        }
     end
 
 end
 
-
+    #wine_block = doc.css("tr td").text
     #wine_name_and_vintage = doc.css("div.table-name span.wineName")[0].text
     #wine_rank = doc.css("td.rank")[0].text
     #winery_name = doc.css("span.sort-text")[0].text
@@ -16,6 +30,6 @@ end
     #wine_vintage = doc.css("td.vintage")[0].text
     #wine_score = doc.css("td.score")[0].text
     #wine_price = doc.css("td.price")[0].text
-    #wine_full_description = doc.css("div.tabel-note")[0].text
-
-    
+    #wine_full_description = doc.css("div.tabel-note")[0].children[0].text.strip.chomp(' —')
+        #(CHANGE DESC ARRAY LOCATION, BUT CHILD STAYS 0)
+end
