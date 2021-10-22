045551f03591d855d9fc281da79837155109d362
Katherine Walendzik <katanwal@icloud.com>
2021-10-22 17:54:26 -0400

    added colorize and cleaned up interface

--- lib/top_wines/cli.rb
+++ lib/top_wines/cli.rb
@@ -2,88 +2,55 @@ class TopWines::CLI
     
     def call
         TopWines::Scraper.scrape_wines
-        puts "\nWelcome to Wine Spectator's Top 100 Wines of 2020!" 
+        puts "\nWelcome to Wine Spectator's Top 100 Wines of 2020!".white.on_black
         start
     end
 
     def start
-        puts "\nPlease select the group of wines you would like to see: \n1-20 \n21-40 \n41-60 \n61-80 \n81-100" 
+        puts "\nPlease select the group of wines you would like to see: \n1-20 \n21-40 \n41-60 \n61-80 \n81-100".black.on_light_white
         input = gets.strip.to_i
 
         print_wines(input)
 
-        puts "Select the corresponding number of the wine of your choice for more details"
+        puts "\nPlease select the corresponding number of the wine of your choice for more details.".black.on_light_white
         input = gets.strip
 
         wine = TopWines::Wine.find(input.to_i)
 
         print_wine(wine)
 
-        puts "Would you like to view another wine? Enter Y or N"
+        puts "\nWould you like to view another wine? Enter Y or N".black.on_light_white
 
         input = gets.strip.downcase
         if input == "y"
             start
         elsif input == "n"
             puts ""
-            puts "Thank you! Have a great day!"
+            puts "\nCheers!".white.on_black
             exit
         else
             puts ""
-            puts "I don't understand that answer."
+            puts "\nI don't understand that answer.".black.on_light_white
             start
         end
     end
 
-    def print_wine(wine)
-        puts "#{wine.rank}. #{wine.winery} #{wine.name}"
-        puts "Rank: #{wine.rank}"
-        puts "Winery: #{wine.winery}"
-        puts "Name: #{wine.name}"
-        puts "Vintage: #{wine.vintage}"
-        puts "Score: #{wine.score}"
-        puts "Price: #{wine.price}"
-        puts "Full Description: #{wine.full_description}"
-    end
-
     def print_wines(from_number)
-        puts "Wines ranked #{from_number} - #{from_number+19}"
+        puts "\nWINES RANKED #{from_number} - #{from_number+19}".black.on_light_white
         TopWines::Wine.all[from_number-1, 20].each.with_index(from_number) do |wine, index|
-            puts "#{index}. #{wine.winery} - #{wine.name}"
+            puts "\n#{index}. #{wine.winery} - #{wine.name}"
         end
     end
 
+    def print_wine(wine)
+        puts "\n#{wine.rank}. #{wine.winery.upcase} - #{wine.name.upcase}".black.on_light_white
+        puts "RANK: #{wine.rank}"
+        puts "WINERY: #{wine.winery}"
+        puts "NAME: #{wine.name}"
+        puts "VINTAGE: #{wine.vintage}"
+        puts "SCORE: #{wine.score}"
+        puts "PRICE: #{wine.price}"
+        puts "FULL DESCRIPTION: #{wine.full_description}"
+    end
+
 end
-
-
-
-
-
-
-
-    #def list_wines # (input arg)
-    #    TopWines::Wine.all.each.with_index(1) do |wine, i|
-    #        puts "#{i}. #{wine.winery} #{wine.name}"
-    #    end
-    #end
-
-
-        #@groups = ["1-20", "21-40", "41-60", "61-80", "81-100"]
-
-        #@groups.each.with_index(1) do |group, index| 
-        #    puts "#{index}. #{group}"
-        #end
-
-    #def get_user_input
-    #    chosen_group = gets.strip.to_i
-    #    list_wines(chosen_group) if valid_input(chosen_group, @groups)
-    #end
-
-    #def valid_input(input, data)
-    #    input.to_i <= data.length && input.to_i > 0
-    #end 
-
-    #def list_wines(chosen_group)
-    #    group = @groups[chosen_group - 1]
-    #    puts "\nWines ranked #{group}:"
-    #end
