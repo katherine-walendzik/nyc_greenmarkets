71df1ae961084732728b41ba59760d84de7b9a70
Katherine Walendzik <katanwal@icloud.com>
2021-10-22 23:41:04 -0400

    split cli methods, added exit command

--- lib/top_wines/cli.rb
+++ lib/top_wines/cli.rb
@@ -3,35 +3,48 @@ class TopWines::CLI
     def call
         TopWines::Scraper.scrape_wines
         puts "\nWelcome to Wine Spectator's Top 100 Wines of 2020!".white.on_black
-        start
+        @input = ""
+        
+        until @input == "exit"
+            start
+            select_wine
+            next_move
+        end
     end
 
     def start
         puts "\nPlease select the group of wines you would like to see: \n1-20 \n21-40 \n41-60 \n61-80 \n81-100".black.on_light_white
         input = gets.strip.to_i
 
-        print_wines(input)
+        print_wines(input) 
+        select_wine
+        next_move
+    end
 
+    def select_wine
         puts "\nPlease select the corresponding number of the wine of your choice for more details.".black.on_light_white
         input = gets.strip
 
         wine = TopWines::Wine.find(input.to_i)
 
-        print_wine(wine)
+        print_wine(wine) 
+    end
 
+    def next_move
         puts "\nWould you like to view another wine? Enter Y or N".black.on_light_white
 
-        input = gets.strip.downcase
-        if input == "y"
+        @input = gets.strip.downcase
+        if @input == "y"
             start
-        elsif input == "n"
-            puts ""
+        elsif @input == "n"
+            puts "\nCheers!".white.on_black
+            exit
+        elsif @input == "exit"
             puts "\nCheers!".white.on_black
             exit
         else
-            puts ""
-            puts "\nI don't understand that answer.".black.on_light_white
-            start
+            puts "\nI'm not sure what you mean. Keep exploring wines, or type 'exit' to leave.".black.on_light_white
+            next_move
         end
     end
 
